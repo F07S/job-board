ad6e1fa7048a8fd3df65ceb6ee88935680c280c5
Francesca Saunders <francescasaunders@MBPdeFrancesca.lan>
2023-01-30 16:11:31 +0100

    second-commit

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,12 +1,14 @@
 const Jobs = (props) => {
   return (
     <>
-      <div className="job-box">
-        <h4>{props.title}</h4>
-        <div className="description">
-          <span>{props.contractType}</span>
-          <span>{props.country}</span>
-          <span>{props.city}</span>
+      <div className={props.className}>
+        <div className="job-box">
+          <h4>{props.title}</h4>
+          <div className="description">
+            <span>{props.contractType}</span>
+            <span>{props.country}</span>
+            <span>{props.city}</span>
+          </div>
         </div>
       </div>
     </>
