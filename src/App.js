a3cb1b7a69f4e8a04d32374d950739adc87b5996
Francesca Saunders <francescasaunders@MBPdeFrancesca.lan>
2023-01-30 15:38:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Jobs from "./components/Job";
+import Title from "./components/Title";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <Title name="The Job Board" />
       </header>
-    </div>
+      <section className="jobs">
+        <Jobs
+          className="red"
+          title="Full Time Sales Associate - Sydney Boutique"
+          contractType="CDI -"
+          country=" Australie -"
+          city=" Sydney"
+        />
+        <Jobs
+          className="green"
+          title="Agent de Sécurité - Pantin"
+          contractType="CDI -"
+          country=" France -"
+          city=" Plantin"
+        />
+        <Jobs
+          className="yellow"
+          title="Responsable d'Atelier (H/F)"
+          contractType="CDD -"
+          country=" France -"
+          city=" Paris"
+        />
+
+        <Jobs
+          className="blue"
+          title="Chef de Projets"
+          contractType="CDD -"
+          country=" France -"
+          city=" Paris"
+        />
+        <Jobs
+          className="beige"
+          title="Développeur React.js"
+          contractType="CDI -"
+          country=" France -"
+          city=" Paris"
+        />
+        <Jobs
+          className="red"
+          title="Sales Associate Stockholm"
+          contractType="CDI -"
+          country=" Suède -"
+          city=" Stockholm"
+        />
+        <Jobs
+          className="green"
+          title="Vendeur/se - Crans Montana"
+          contractType="CDI -"
+          country=" Suisse -"
+          city=" Crans-Montana"
+        />
+        <Jobs
+          className="yellow"
+          title="CRM & Data Quality Analyst"
+          contractType="CDI -"
+          country=" USA -"
+          city=" New York"
+        />
+        <Jobs
+          className="blue"
+          title="Infirmier (H/F)"
+          contractType="CDI -"
+          country=" France -"
+          city=" Pantin"
+        />
+      </section>
+    </>
   );
 }
 
